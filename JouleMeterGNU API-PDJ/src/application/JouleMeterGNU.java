ad8a0da53aa6db334b9be58d350ffa4dcb6d1698
Tiaraju Smaneoto <tiaraju@dori.lsd.ufcg.edu.br>
2013-09-02 17:17:58 -0300

    creating predictor

--- JouleMeterGNU API-PDJ/src/application/JouleMeterGNU.java
+++ JouleMeterGNU API-PDJ/src/application/JouleMeterGNU.java
@@ -11,6 +11,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
+import predictor.Predictor;
+
 import memory.MemoryEnergyMeter;
 import memory.MemoryMonitor;
 import memory.MemoryWorkLoadGenerator;
@@ -61,6 +63,7 @@ public class JouleMeterGNU{
 	private int actualDisk;
 	private double[] memory;
 	double maxDisk ,minDisk ,maxWritten,minWritten ,maxVideo,minVideo;
+	private static Predictor predictor;
 
 	private FileWriter dataTime;
 
@@ -256,211 +259,8 @@ public class JouleMeterGNU{
 
 	}
 
-	//obtem os valores de como cada componente esta se comportando no momento ( cpu,disco,memoria)
-	private void obtainActualSituation(){
-		//condição atual da CPU
-		this.actualFreq=0;
-		CPUMonitor cpuMonitor= new CPUMonitor();
-		this.actualCpus =(int)Math.round((cpuMonitor.getNumberOfCPUs()*((double)cpuMonitor.getCPULoad())/100));
-		System.out.println(actualCpus);
-		int numberOfCpus;
-		if(actualCpus == 0){numberOfCpus=actualCpus;}else{  numberOfCpus=actualCpus-1;}
-		double freq=cpuMonitor.getActualFreq(numberOfCpus);
-		List<Double> freqs= new ArrayList<Double>();
-		for(int i=cpuMonitor.getCPUFrequencyLevels().length-1;i>=0;i--){
-			freqs.add(cpuMonitor.getCPUFrequencyLevels()[i]);
-		}
-		actualFreq = freqs.indexOf(freq);
-		 
-		//condição atual do disco
-		DiskMonitor diskMonitor = new DiskMonitor();
-		this.actualDisk=diskMonitor.getActualWrittenPerSecond();
-		 
-		//condição atual da memoria
-		MemoryMonitor memoryMonitor = new MemoryMonitor();
-		actualMemory =memoryMonitor.getUsedMemory();
-
-		// TODO condição atual do video
-	}
-	//carrega os dados obtidos na calibragem
-	private void loadData(){
-		this.memory=new double[5];
-		BufferedReader reader;
-		String userName = System.getProperty("user.name");
-		
-		try {
-			memoryReader= new FileReader("/home/"+userName+"/memoryResult.txt");
-			diskReader= new FileReader("/home/"+userName+"/JMGProject/disk.txt");
-			cpuReader = new FileReader("/home/"+userName+"/JMGProject/cpu.txt");
-			videoReader= new FileReader("/home/"+userName+"/JMGProject/video.txt");
-		} catch (FileNotFoundException e) {
-			System.err.println(e.getMessage());
-		}
-
-		//valores da memoria
-		reader = new BufferedReader(memoryReader);
-		String line=null;
-		try {
-			line = reader.readLine();
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-		while(line!=null){
-			System.out.println(line);
-			if(line.substring(0,5).contains("50%")) {
-				System.out.println("foi 50");
-				System.out.println("colocou: "+line.split(" ")[line.split(" ").length-1]);
-				memory[2]=Double.parseDouble(line.split(" ")[line.split(" ").length-1]);
-			}
-			else if(line.substring(0,5).contains("25%")){
-				System.out.println("foi 25");
-				System.out.println("colocou: "+line.split(" ")[line.split(" ").length-1]);
-				memory[1]=Double.parseDouble(line.split(" ")[line.split(" ").length-1]);
-			}
-			else if(line.substring(0,5).contains("75%")){
-				System.out.println("foi 75");
-				System.out.println("colocou: "+line.split(" ")[line.split(" ").length-1]);
-				memory[3]=Double.parseDouble(line.split(" ")[line.split(" ").length-1]);
-			}
-			else if(line.substring(0,5).contains("100%")) {
-				System.out.println("foi 100");
-				System.out.println("colocou: "+line.split(" ")[line.split(" ").length-1]);
-				memory[4]=Double.parseDouble(line.split(" ")[line.split(" ").length-1]);
-			}
-			else if(line.substring(0,5).contains("0%")){
-				System.out.println("foi 0");
-				System.out.println("colocou: "+line.split(" ")[line.split(" ").length-1]);
-				memory[0]=Double.parseDouble(line.split(" ")[line.split(" ").length-1]);
-			}else{
-				break;
-			}
-			try {
-				line=reader.readLine();
-			} catch (IOException e) {
-				System.out.println("naoleualinha");
-			}
-		}
-		try{
-			reader.close();
-			memoryReader.close();
-			for(Double d:memory){
-				System.out.println(d);
-			}
-		}catch(Exception e){
-		}
-		//valores do disco
-		reader=new BufferedReader(diskReader);
-		try {
-			line = reader.readLine();
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-		while(line!=null){
-			if(line.contains("ON")) maxDisk=Double.parseDouble(line.split(" ")[1]);
-			if(line.contains("OFF")) minDisk=Double.parseDouble(line.split(" ")[1]);
-			if(line.contains("MAXWS")) maxWritten=Double.parseDouble(line.split(" ")[1]);
-			if(line.contains("MINWS")) minWritten=Double.parseDouble(line.split(" ")[1]);
-			try {
-				line=reader.readLine();
-			} catch (IOException e) {
-				System.err.println(e.getMessage());
-			}
-		}
-		//valores do video
-		reader= new BufferedReader(videoReader);
-		try {
-			line = reader.readLine();
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-		while(line!=null){
-			if(line.contains("ON")) maxVideo=Double.parseDouble(line.split(" ")[1]);
-			if(line.contains("OFF")) minVideo=Double.parseDouble(line.split(" ")[1]);
-			try {
-				line=reader.readLine();
-			} catch (IOException e) {
-				System.err.println(e.getMessage());
-			}
-		}	 
-
-	}
-	//associa a condiçao atual do componente com o obtido na calibragem
 	
-	private double[] associateValues(){
-		double[] result=new double[5];
-		//cpu
-		CPUEnergyMeter m = new CPUEnergyMeter(0, null, null, null);
-		double[][] cpuMatrix=m.getMatrixOfConsumption();
-		result[0]= cpuMatrix[actualCpus][actualFreq];
-		 
-		//memoria
-		MemoryMonitor memoryMonitor = new MemoryMonitor();
 	
-		System.out.println("");
-		int percentageOfUse=roundMemory(actualMemory, memoryMonitor.getTotalMemory());
-		
-		result[1]=memory[percentageOfUse/25];
-				//memory[(roundMemory(actualMemory, memoryMonitor.getTotalMemory()))/25];
-		
-		//disco
-		if((maxWritten-actualDisk)<(actualDisk-minWritten)){
-			result[2]=maxDisk*(actualDisk/maxWritten);
-		}else{
-			result[2]=minDisk*(actualDisk/minWritten);
-		}
-		return result;
-	}
-
-	private void getValidationData(int type, String hostname, String username, String password){
-		String userName = System.getProperty("user.name");
-		double[] components=null; 
-		try {
-			EnergyMeter meter = new EnergyMeter(type, hostname, username, password);
-			FileWriter validationFile = new FileWriter("/home/"+userName+"/JMGProject/validation.txt");
-			obtainActualSituation();
-			components=associateValues();
-			validationFile.append("CPU: Estimate - "+components[0]+" f "+actualFreq+" c "+actualCpus+"\n");
-			obtainActualSituation();
-			components=associateValues();
-			validationFile.append("MEMORY: Estimate - "+components[1]);
-			obtainActualSituation();
-			components=associateValues();
-			validationFile.append("DISK: Estimate - "+components[2]);
-			validationFile.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	private static int roundMemory(double actualMemory,double totalMemory){ 
-		double percentageOfUse =actualMemory*100.0/totalMemory;
-		int result=0;
-		if(percentageOfUse < 50){
-			if(percentageOfUse<=12){
-				result= 0;
-			}else if(percentageOfUse<=34){
-				result= 25;
-			}else{
-				result= 50;
-			}
-
-		}else if(percentageOfUse > 50){
-			if(percentageOfUse<=62){
-				result= 50;
-			}else if(percentageOfUse<=84){
-				result= 75;
-			}else{
-				result= 100;
-			}
-
-		}else{
-			result= 50;
-		}
-
-
-		return result;
-	}
 
 	private void connect(String hostname, String username, String password) throws IOException {
 		serverConnection = new Connection(hostname);
@@ -504,7 +304,7 @@ public class JouleMeterGNU{
 		case YOKOGAWA:
 			System.out.println("Please, type your hostname: ");
 			hostname = input.next();
-			input.close();
+			input.close();	
 			meter.calibrate(YOKOGAWA, hostname,null,null);
 			calibrate = true;
 			break;
@@ -513,14 +313,12 @@ public class JouleMeterGNU{
 		meter.calibrate(0, null,null, null);
 		FileWriter writer = new FileWriter("/home/tiaraju/memoryValidation.txt");
 		MemoryMonitor monitor = new MemoryMonitor();
-		meter.loadData();
 		MemoryWorkLoadGenerator work = new MemoryWorkLoadGenerator();
 		for(int i=0;i<=100;i+=10){
 			long memory=Math.round(monitor.getFreeMemory()*(i/100.0));
 			Runtime.getRuntime().exec("stress --vm 1 --vm-bytes "+memory+"K --vm-keep");
 			Thread.sleep(30000);
-			meter.obtainActualSituation();
-			writer.append(i+"-"+memory+": "+new Date().toString().substring(11,19)+"- actualMem: "+actualMemory+"- percentage of use: "+roundMemory(actualMemory, monitor.getTotalMemory())+"-power: "+meter.associateValues()[1]+"\n");
+			writer.append(i+"-"+memory+": "+new Date().toString().substring(11,19)+"- actualMem: "+actualMemory+"- percentage of use: "+predictor.currentUse()[2]+"-power: "+predictor.predict()[1]+"\n");
 			Runtime.getRuntime().exec("killall stress").waitFor();
 			work.resetStress();
 			Thread.sleep(5000);
